17899a9faccdd60c261c348419000307715fba4b
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-09-18 02:23:44 -0300

    Primeiro requisito

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,8 +1,14 @@
 require('../mocks/fetchSimulator');
+import { expect } from '@jest/globals';
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
+  it('se é uma funçao' = () => {
+    expect.assertions(3);
+    expect(typeof (fetchItem)).toBe('function');
+    expect()
+  })
   // implemente seus testes aqui
   fail('Teste vazio');
 });
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,33 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('se é uma funçao', () => {
+    expect.assertions(1);
+    expect(typeof (fetchProducts)).toBe('function');
+  });
+
+  it('se a funçao fetch foi chamada', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('se a função fetch foi chamada com o endpoint correto', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('se o retorno é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect.assertions(1);
+    const result = await fetchProducts('computador');
+    expect(result).toBe(computadorSearch);
+  });
+
+  it('se o retorno da função sem argumento é um erro com a mensagem: \'You must provide an url\'', async () => {
+    expect.assertions(1);
+    expect(await fetchProducts(' ')).toBe(new Error('You must provide an url'));
+  });
 });
+
+  //  fail('Teste vazio');
