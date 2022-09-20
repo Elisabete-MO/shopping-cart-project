a0501aadb99d8fcc9aeb5f9850f70c6ef5886e90
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-09-20 01:33:49 -0300

    Sexto e setimo requisito

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,22 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('se é uma funçao', () => {
+    // expect.assertions(1);
+    expect(typeof (getSavedCartItems)).toBe('function');
+    });
+  
+  it('se o método localStorage.setItem é chamado', () => {
+    // expect.assertions(1);
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  
+  it('se a função o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave `cartItems` e o segundo sendo o valor passado como argumento para saveCartItems', () => {    
+    // expect.assertions(1);
+    getSavedCartItems('cartItems, item');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems, item');
+  });
 });
+  // fail('Teste vazio');
+
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,22 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('se é uma funçao', () => {
+    // expect.assertions(1);
+    expect(typeof (saveCartItems)).toBe('function');
+    });
+  
+  it('se o método localStorage.setItem é chamado', () => {
+    // expect.assertions(1);
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  
+  it('se a função o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave `cartItems` e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    // expect.assertions(1);
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems, item');
+  });
 });
+
+// fail('Teste vazio');
